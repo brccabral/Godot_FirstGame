40294525b6554a9934b56300117069521e75fa27
Bruno Cabral <cabral.brc@gmail.com>
2024-06-03 20:34:16 -0700

    objects collide with coin

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,11 +1,6 @@
 extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print("I'm a coin.")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_body_entered(body):
+#	any physics object will trigger this event (platforms)
+	print("+1 coin!")
