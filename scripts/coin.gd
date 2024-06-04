87e6029de5904aa6033a3048126c1ff3ea710552
Bruno Cabral <cabral.brc@gmail.com>
2024-06-03 23:11:56 -0700

    pickup coin sound

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -2,7 +2,16 @@ extends Area2D
 
 # using % because GameManger is marked as UNIQUE in our scene
 @onready var game_manager = %GameManager
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(_body):
 	game_manager.add_point()
-	queue_free()
+	# we use the animation "pickup" to
+	# - invisible coin
+	# - disable collision
+	# - play sound
+	# - and then call queue_free()
+	# we do this using animation because when calling queue_free()
+	# directly it stops the sound. So, we use animation to give some time
+	# to play the sound before removing the coin from memory
+	animation_player.play("pickup")
