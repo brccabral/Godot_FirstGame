10f16edce49698dd63fb3a291b9c6e303ae37e55
Bruno Cabral <cabral.brc@gmail.com>
2024-06-03 22:03:31 -0700

    player killed effect

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,9 +3,20 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
+	# body is the player because it is the only object that enters killzone
 	print("You died!")
+
+	# slows the engine when player is killed
+	Engine.time_scale = 0.5
+
+	# remove the collision from the player so it falls down the ground
+	# when it gets killed
+	body.get_node("CollisionShape2D").queue_free()
+
+	# triggers scene restart on func "_on_timer_timeout"
 	timer.start()
 
 
 func _on_timer_timeout():
+	Engine.time_scale = 1 # resets game speed
 	get_tree().reload_current_scene()
