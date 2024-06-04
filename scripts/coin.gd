1a63faac1a8dc1c82beae98b44f2b065804f2894
Bruno Cabral <cabral.brc@gmail.com>
2024-06-03 22:43:00 -0700

    coin adds score to game manager

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+# using % because GameManger is marked as UNIQUE in our scene
+@onready var game_manager = %GameManager
 
 func _on_body_entered(body):
-	print("+1 coin!")
+	game_manager.add_point()
 	queue_free()
